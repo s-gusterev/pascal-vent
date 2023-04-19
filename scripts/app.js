c45e8631f88539ffcb7c7302ffa821c9982fe051
Sergei Gusterev <copywriter-sg@yandex.ru>
2023-04-19 09:38:04 +0300

    feat: swiper

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,24 @@
 import 'normalize.css';
 import '../styles/main.css';
-
 import showTab from './tabs';
+// core version + navigation, pagination modules:
+
+// init Swiper:
 
 showTab(0);
+
+// const swiper = new Swiper('.mySwiper', {
+//   // configure Swiper to use modules
+//   // modules: [Navigation],
+//   // Optional parameters
+//   direction: 'vertical',
+//   loop: true,
+
+//   // Navigation arrows
+//   navigation: {
+//     nextEl: '.portfolio__button-next',
+//     prevEl: '.portfolio__button-prev',
+//   },
+// });
+
+// console.log(swiper);

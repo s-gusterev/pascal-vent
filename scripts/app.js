834b10c690c261b1d2c0e4e055a04f6ca78df90d
Sergei Gusterev <copywriter-sg@yandex.ru>
2023-05-08 09:28:57 +0300

    feat: popup and fixed menu

--- scripts/app.js
+++ scripts/app.js
@@ -6,10 +6,44 @@ import $ from 'jquery';
 import 'slick-carousel/slick/slick';
 import showTab from './tabs';
 
+const popup = document.querySelector('.popup');
+const headerButton = document.querySelector('.header__button');
+const popupClose = document.querySelector('.popup__close');
+const headerWrapper = document.querySelector('.header__wrapper');
+const servicesPaddingTop = 150;
+const services = document.querySelector('.services');
+const fixedHeader = services.offsetTop - servicesPaddingTop;
+
+const openPopup = (popup) => {
+  popup.classList.add('popup_open');
+  document.addEventListener('keydown', closePopupEsc);
+  document.body.classList.add('modal-open');
+  headerWrapper.style.visibility = 'hidden';
+};
+
+const closePopup = (popup) => {
+  popup.classList.remove('popup_open');
+  document.removeEventListener('keydown', closePopupEsc);
+  document.body.classList.remove('modal-open');
+  headerWrapper.style.visibility = 'visible';
+};
+
+const closePopupEsc = (evt) => {
+  if (evt.key === 'Escape') {
+    const popup = document.querySelector('.popup_open');
+    closePopup(popup);
+  }
+};
+
+const popupCloseOverlay = (event, popup) => {
+  if (event.target === event.currentTarget) {
+    closePopup(popup);
+  }
+};
+
 showTab(0);
 
 $('.portfolio__slider-wrapper').slick({
-  // infinite: true,
   slidesToShow: 1,
   slidesToScroll: 1,
   variableWidth: true,
@@ -35,7 +69,6 @@ $('.portfolio__slider-wrapper').on(
 );
 
 $('.clients__slides').slick({
-  // infinite: true,
   slidesToShow: 1,
   slidesToScroll: 1,
   variableWidth: true,
@@ -44,3 +77,21 @@ $('.clients__slides').slick({
   cssEase: 'ease',
   speed: 500,
 });
+
+const visibleScrollMenu = () => {
+  if (window.pageYOffset > fixedHeader) {
+    headerWrapper.classList.add('header__wrapper_fixed');
+    services.style.paddingTop = '150px';
+  } else {
+    headerWrapper.classList.remove('header__wrapper_fixed');
+    services.style.paddingTop = '100px';
+  }
+};
+
+window.onscroll = function () {
+  visibleScrollMenu();
+};
+
+headerButton.addEventListener('click', () => openPopup(popup));
+popupClose.addEventListener('click', () => closePopup(popup));
+popup.addEventListener('click', (event) => popupCloseOverlay(event, popup));

11d1a2f93d09e13d008175430effe12a6fcff93d
Sergei Gusterev <copywriter-sg@yandex.ru>
2023-05-12 05:20:13 +0300

    plugin deploy

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,8 @@
 // vite.config.js
+import { ghPages } from 'vite-plugin-gh-pages';
+
+/** @type {import('vite').UserConfig} */
 export default {
   base: '/pascal-vent/',
+  plugins: [ghPages()],
 };
